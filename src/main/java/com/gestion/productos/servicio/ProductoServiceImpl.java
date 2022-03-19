4e10f5a8fd155e385ad8335d964257ed48ced292
mignarvaez <migueln77@hotmail.es>
2022-03-18 23:29:21 -0500

    Configuración estructura proyecto y paginación
    Se realiza configuración básica a la estructura del proyecto, asi como
    la creacion de la logica para la paginación.

---
+++ src/main/java/com/gestion/productos/servicio/ProductoServiceImpl.java
@@ -0,0 +1,85 @@
+package com.gestion.productos.servicio;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gestion.productos.entidades.Producto;
+import com.gestion.productos.repositorios.ProductoRepository;
+
+/**
+ * Implementación del servicio de empleados Tener en cuenta que no se va a crear
+ * una API rest, sino que se trabajara con un controlador tradicional.
+ * 
+ * @author Migue
+ *
+ */
+public class ProductoServiceImpl implements ProductoService {
+
+	// -------------------------------------------------------------
+	// Atributos
+	// -------------------------------------------------------------
+
+	/**
+	 * Vincula el repositorio
+	 */
+	@Autowired
+	private ProductoRepository productoRepository;
+
+	// -------------------------------------------------------------
+	// Métodos
+	// -------------------------------------------------------------
+
+	/**
+	 * Retorna la lista de productos. Se indica con la anotación Transactional
+	 * (permite commit rollback) que es solo de lectura
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public List<Producto> findAll() {
+		return (List<Producto>) productoRepository.findAll();
+	}
+
+	/**
+	 * Retorna la lista de productos paginada.
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Producto> findAll(Pageable pageable) {
+		return productoRepository.findAll(pageable);
+	}
+
+	/**
+	 * Permite guardar un producto es transaccional y permite operaciones de
+	 * escritura lectura
+	 */
+	@Override
+	@Transactional
+	public void save(Producto producto) {
+		productoRepository.save(producto);
+	}
+
+	/**
+	 * Busca un producto según id, es transaccional de solo lectura.
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public Producto findOne(Long id) {
+
+		// En caso de no encontrar un producto por el id suministrado retorna null
+		return productoRepository.findById(id).orElse(null);
+	}
+
+	/**
+	 * Elimina un producto según id.
+	 */
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		productoRepository.deleteById(id);
+	}
+
+}
---
+++ src/main/java/com/gestion/productos/util/paginacion/PageRender.java
@@ -0,0 +1,160 @@
+package com.gestion.productos.util.paginacion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+/**
+ * Renderiza una pagina, es una clase de tipo generico <T>: Indica que hara
+ * referencia a una clase
+ * 
+ * @author Migue
+ *
+ */
+public class PageRender<T> {
+
+	/**
+	 * La url
+	 */
+	private String url;
+
+	/**
+	 * La pagina
+	 */
+	private Page<T> page;
+
+	/**
+	 * El total de paginas
+	 */
+	private int totalPaginas;
+
+	/**
+	 * El número de elemtnos por pagina, ojo no corresponde a la cantidad de
+	 * elementos que se veran por pagina, eso se hace desde el repositorio. Esta
+	 * paginacion hace referencia a los botones de la parte superior o inferior que
+	 * se indican en el paginador. (... 1 2 3 4 5 ...)
+	 */
+	private int numElementosPorPagina;
+
+	/**
+	 * Pagina actual en la que se encuentra el usuario
+	 */
+	private int paginaActual;
+
+	/**
+	 * Las paginas a renderizar.
+	 */
+	private List<PageItem> paginas;
+
+	/**
+	 * Constructor del page render
+	 * 
+	 * @param url  la url de la pagina
+	 * @param page la pagina a renderizar
+	 */
+	public PageRender(String url, Page<T> page) {
+
+		// Se inicializa url y pagina
+		this.url = url;
+		this.page = page;
+
+		// Se inicializan las paginas
+		this.paginas = new ArrayList<PageItem>();
+
+		// Se especifica el número de elementos por página.
+		numElementosPorPagina = 5;
+
+		// Se obtiene el total de paginas
+		totalPaginas = page.getTotalPages();
+
+		// Se obtiene la página uno, como los indices inician desde 0 se agrega 1
+		paginaActual = page.getNumber() + 1;
+
+		/**
+		 * Algoritmo que actualiza el paginador
+		 */
+		int desde, hasta;
+		if (totalPaginas <= numElementosPorPagina) {
+			desde = 1;
+			hasta = totalPaginas;
+		} else {
+			if (paginaActual <= numElementosPorPagina / 2) {
+				desde = 1;
+				hasta = numElementosPorPagina;
+			} else if (paginaActual >= totalPaginas - numElementosPorPagina / 2) {
+				desde = totalPaginas - numElementosPorPagina + 1;
+				hasta = numElementosPorPagina;
+			} else {
+				desde = paginaActual - numElementosPorPagina / 2;
+				hasta = numElementosPorPagina;
+			}
+		}
+
+		/**
+		 * Crea las paginas de manera actualizada
+		 */
+		for (int i = 0; i < hasta; i++) {
+			paginas.add(new PageItem(desde + i, paginaActual == desde + i));
+		}
+
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getTotalPaginas() {
+		return totalPaginas;
+	}
+
+	public void setTotalPaginas(int totalPaginas) {
+		this.totalPaginas = totalPaginas;
+	}
+
+	public int getPaginaActual() {
+		return paginaActual;
+	}
+
+	public void setPaginaActual(int paginaActual) {
+		this.paginaActual = paginaActual;
+	}
+
+	public List<PageItem> getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(List<PageItem> paginas) {
+		this.paginas = paginas;
+	}
+
+	/**
+	 * Método que indica si es la ultima pagina
+	 * 
+	 * @return
+	 */
+	public boolean isLast() {
+		return page.isLast();
+	}
+
+	/**
+	 * Método que indica si la página tiene siguiente
+	 * 
+	 * @return
+	 */
+	public boolean isHasNext() {
+		return page.hasNext();
+	}
+
+	/**
+	 * Método que indica si la ágina tiene anterior
+	 * @return
+	 */
+	public boolean isHasPrevious() {
+		return page.hasPrevious();
+	}
+}
